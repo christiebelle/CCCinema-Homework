2dd57d9fe30aba5c5c703178f5e9e425a337bc18
Christiebelle <cslester@outlook.com>
2018-02-09 14:18:44 +0000

    CRUD functions added for films

--- console.rb
+++ console.rb
@@ -6,6 +6,7 @@ require_relative('models/films')
 require_relative('models/tickets')
 
 Customer.delete_all
+Film.delete_all
 
 customer1 = Customer.new({
   'name'=>'Christie',
@@ -13,7 +14,7 @@ customer1 = Customer.new({
   })
 
 customer2 = Customer.new({
-  'name'=>'Sarah',
+  'name'=>'Sara',
   'funds'=> '20'
   })
 
@@ -27,14 +28,60 @@ customer4 = Customer.new({
   'funds'=> '25'
   })
 
+customer5 = Customer.new({
+  'name'=>'Barry',
+  'funds'=> '25'
+  })
+
+customer6 = Customer.new({
+  'name'=>'Kara',
+  'funds'=> '25'
+  })
+
+customer7 = Customer.new({
+  'name'=>'Joe',
+  'funds'=> '25'
+  })
+
 customer1.save
 customer2.save
 customer3.save
 customer4.save
+customer5.save
+customer6.save
+customer7.save
 
 customer3.name = 'John'
 customer3.update
 
+film1 = Film.new({
+  'title' => 'WonderWoman',
+  'price' => '10',
+  'show_time' => '8.00'
+  })
+
+film2 = Film.new({
+  'title' => 'Lego Batman Movie',
+  'price' => '8',
+  'show_time' => '7.00'
+  })
+
+film3 = Film.new({
+  'title' => 'Life of Pi',
+  'price' => '10',
+  'show_time' => '9.00'
+  })
+
+film4 = Film.new({
+  'title' => 'Silver Linings Playbook',
+  'price' => '10',
+  'show_time' => '8.30'
+  })
+
+film1.save
+film2.save
+film3.save
+film4.save
 
 
 binding.pry
--- models/films.rb
+++ models/films.rb
@@ -1,1 +1,48 @@
 require_relative('../db/sql_runner')
+
+class Film
+
+  attr_reader :id
+  attr_accessor :title, :price, :show_time
+
+  def initialize ( options )
+    @id = options[id].to_i if options[id]
+    @title = options['title']
+    @price = options['price'].to_i
+    @show_time = options['show_time'].to_i
+  end
+
+  def save()
+    sql = "INSERT INTO films (title, price, show_time) VALUES ($1, $2, $3) RETURNING id"
+    values = [@title, @price, @show_time]
+    film = SqlRunner.run(sql,values).first
+    @id = film['id'].to_i
+  end
+
+  def delete_item()
+    sql = "DELETE FROM films WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def update()
+    sql = "UPDATE films SET (title, price, show_time) = ($1, $2, $3) WHERE id = $3"
+    values = [@title, @price, @show_time, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM films"
+    values = []
+    SqlRunner.run(sql, values)
+  end
+
+  def self.view_all()
+    sql = "SELECT * FROM films"
+    values = []
+    movie = SqlRunner.run(sql, values)
+    result = movie.map {|show| Film.new(show)}
+    return result
+  end
+
+end
