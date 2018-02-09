dabdce48e95ec19e6f5d33a4e891690dee285041
Christiebelle <cslester@outlook.com>
2018-02-09 14:32:36 +0000

    CRUD functions added for tickets

--- console.rb
+++ console.rb
@@ -84,5 +84,50 @@ film3.save
 film4.save
 
 
+ticket1 = Ticket.new({
+  'film_id' => film1.id,
+  'customer_id' => customer2.id
+  })
+
+ticket2 = Ticket.new({
+  'film_id' => film1.id,
+  'customer_id' => customer6.id
+  })
+
+ticket3 = Ticket.new({
+  'film_id' => film2.id,
+  'customer_id' => customer5.id
+  })
+
+ticket4 = Ticket.new({
+  'film_id' => film3.id,
+  'customer_id' => customer1.id
+  })
+
+ticket5 = Ticket.new({
+  'film_id' => film3.id,
+  'customer_id' => customer4.id
+  })
+
+ticket6 = Ticket.new({
+  'film_id' => film4.id,
+  'customer_id' => customer7.id
+  })
+
+ticket7 = Ticket.new({
+  'film_id' => film3.id,
+  'customer_id' => customer3.id
+  })
+
+
+ticket1.save
+ticket2.save
+ticket3.save
+ticket4.save
+ticket5.save
+ticket6.save
+ticket7.save
+
+
 binding.pry
 nil
--- models/tickets.rb
+++ models/tickets.rb
@@ -1,1 +1,47 @@
 require_relative('../db/sql_runner')
+
+class Ticket
+
+  attr_reader :id
+  attr_accessor :film_id, :customer_id
+
+  def initialize ( options )
+    @id = options[id].to_i if options[id]
+    @film_id = options['film_id']
+    @customer_id = options['customer_id'].to_i
+  end
+
+  def save()
+    sql = "INSERT INTO tickets (film_id, customer_id) VALUES ($1, $2) RETURNING id"
+    values = [@film_id, @customer_id]
+    film = SqlRunner.run(sql,values).first
+    @id = film['id'].to_i
+  end
+
+  def delete_item()
+    sql = "DELETE FROM tickets WHERE id = $1"
+    values = [@id]
+    SqlRunner.run(sql, values)
+  end
+
+  def update()
+    sql = "UPDATE tickets SET (film_id, customer_id, show_time) = ($1, $2, $3) WHERE id = $3"
+    values = [@film_id, @customer_id, @id]
+    SqlRunner.run(sql, values)
+  end
+
+  def self.delete_all()
+    sql = "DELETE FROM tickets"
+    values = []
+    SqlRunner.run(sql, values)
+  end
+
+  def self.view_all()
+    sql = "SELECT * FROM tickets"
+    values = []
+    stubs = SqlRunner.run(sql, values)
+    result = stubs.map {|stub| Ticket.new(stub)}
+    return result
+  end
+
+end
